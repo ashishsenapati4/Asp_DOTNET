0c20b2a8a15cb44b60e032be1f44466c9396437d
ashishsenapati4 <ashissenapati111@gmail.com>
2025-09-30 21:34:46 +0530

    StronglyTypedViewsAspCore/ added

---
+++ StronglyTypedViewsAspCore/StronglyTypedViewsAspCore/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace StronglyTypedViewsAspCore.Models
+{
+    public class Employee
+    {
+            public int EmpId { get; set; }
+
+            public string EmpName { get; set; }
+
+            public string EmpDesig { get; set; }
+
+            public int EmpSal { get; set; }
+        
+    }
+}
