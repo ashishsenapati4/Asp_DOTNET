d4e16355f31cbd275dedc24c4cd1ce57cc0bb01c
ashishsenapati4 <ashissenapati111@gmail.com>
2025-09-30 21:34:42 +0530

    ModelDataUsingViewData/ added

---
+++ ModelDataUsingViewData/ModelDataUsingViewData/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelDataUsingViewData.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpDesig { get; set; }
+
+        public int EmpSal {  get; set; }
+    }
+}
