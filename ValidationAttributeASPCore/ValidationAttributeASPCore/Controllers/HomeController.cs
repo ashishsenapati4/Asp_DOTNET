e0dc8b6822c3173755ee67fda0f48feb2f2d6fcc
ashishsenapati4 <ashissenapati111@gmail.com>
2025-09-30 21:34:47 +0530

    ValidationAttributeASPCore/ added

---
+++ ValidationAttributeASPCore/ValidationAttributeASPCore/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ValidationAttributeASPCore.Models;
+
+namespace ValidationAttributeASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student stu)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        //[HttpPost]
+        //public string Index(Student std)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        return "Name is: "+std.Name;
+        //    }
+        //    else
+        //    {
+        //        return "Validation failed";
+        //    }
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
